e96c065435b637359f1189752c214bd87abd174f
SoerPirate <72311416+SoerPirate@users.noreply.github.com>
2022-02-07 01:30:47 +0400

    1
    1

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -120,7 +120,7 @@ public class GameController : MonoBehaviour
             foreach (var player in playerCharacter) {
                 if (!player.IsDead()) 
                 {
-                    if (currentTarget == null || currentTarget.IsDead())
+                    if (currentTarget == null || currentTarget.IsDead())  // добавил чтобы не искал новую цель, если уже есть цель (пока она не мертва)
                         currentTarget = FirstAliveCharacter(enemyCharacter);
                     
                     if (currentTarget == null)
